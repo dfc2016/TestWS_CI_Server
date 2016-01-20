5119988939d4ada94a39ff1e75ca00d78205a22c
diego corrada <diego.corrada@valmar.com.pe>
2016-01-20 19:19:45 +0000

    Initial version consume WS from CI Server Changes made by diego corrada (diego.corrada@valmar.com.pe).

--- Test WS/components/home/index.js
+++ Test WS/components/home/index.js
@@ -2,10 +2,68 @@
 
 app.home = kendo.observable({
     onShow: function() {},
-    afterShow: function() {}
+    afterShow: function() {
+        
+     // var dataSource = new kendo.data.DataSource({
+     //      transport: {
+     //        read: {
+     //          url: "http://digiweb-online.com/ci/index.php/servizioweb-test",
+     //          dataType: "json"
+     //        }
+     //      }
+     //  });
+     //  dataSource.fetch(function(){
+     //      var data = this.data();
+     //      console.log("DFC >> data length: "+data.length);          
+     //  });
+        
+    }
 });
 
 // START_CUSTOM_CODE_home
 // Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
-
+function loadNews()
+{
+    var dsNews = new kendo.data.DataSource({
+        transport: {
+            // OK funziona, ottimizzare per grandi vol. di dati || paginazione
+            // Parametrizzare la URL con una variabile idUsuario
+            read: {
+                url: "http://digiweb-online.com/ci/index.php/servizioweb-test",
+                dataType: "json"
+            },
+        },
+        schema: {
+            model: {
+                id: "id",
+                fields: {
+                    id: {
+                        editable: false,
+                        nullable: true,
+                        type: "string"
+                    },
+                    title: {
+                        type: "string"
+                    },
+                }
+            }
+        },
+        requestStart: function (e) {
+            console.log("DFC >>> Request Start");
+        },
+        requestEnd: function (e) {
+            console.log("DFC >>> Request End");
+        },
+        error: function (e) {            
+            alert("El Servicio no esta Disponible.");
+        }
+    });
+    
+    dsNews.fetch(function () {
+        $("#lstNews").kendoMobileListView({
+                dataSource: dsNews,
+                template: kendo.template($("#tmpLstNews").html()), 
+        });
+    });
+}
 // END_CUSTOM_CODE_home
