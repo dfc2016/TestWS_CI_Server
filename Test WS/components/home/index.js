a756c904b988b0854e3fe9a92602615c63a1a96c
diego corrada <diego.corrada@gmail.com>
2016-03-08 15:02:39 +0000

    Implemented MVVM pattern in one file see at view.html and look up for startHome and banner views Changes made by diego corrada (diego.corrada@gmail.com).

--- Test WS/components/home/index.js
+++ Test WS/components/home/index.js
@@ -22,6 +22,58 @@ app.home = kendo.observable({
 
 // START_CUSTOM_CODE_home
 // Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+(function (parent) {
+    var homeModel = kendo.observable({
+        nuevaFx: function () {            
+            app.mobileApp.navigate('#components/home/nuevaFx.html');
+        },
+        terminar: function () {            
+            app.mobileApp.navigate('#components/home/view.html');
+        },
+        testPost: function () {            
+            console.log("DFC >>> test post...");
+            
+            /*
+            $.ajax({
+                url: "http://digiweb-online.com/ci/servizioweb.hello?name=Rocco",
+                type: "post",
+                async: false,
+                success: function (datos) {
+                    console.log("DFC >>> post success: "+datos);
+                },
+                error: function () {
+                    
+                }
+            });
+        	*/
+            
+            $.ajax({
+                url: "http://digiweb-online.com/ci/servizioweb.hellodos",
+                type: "post",
+                data: {
+                    id: "321",
+                },
+                async: false,
+                success: function (datos) {                    
+                    console.log("DFC >>> post success: "+datos);
+                },
+                error: function () {
+                    
+                }
+            });
+  
+        },
+        banner: function () {            
+            app.mobileApp.navigate("#banner"); // the id of the local view
+        },
+        start: function () {            
+            app.mobileApp.navigate("#startHome"); // the id of the local view
+        },
+    });
+    parent.set('homeModel', homeModel);
+    
+})(app.home);
+
 function loadNews()
 {
     var dsNews = new kendo.data.DataSource({
